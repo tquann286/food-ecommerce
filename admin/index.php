0fc502f730ee29e3e919f0cad90a0af54186dab4
tquann286 <quantrung286@gmail.com>
2023-11-19 00:48:09 +0700

    Add admin dashboard, menu, footer, and CSS files.
    - Added `admin/index.php` file to create the admin dashboard.
    - Included `partials/menu.php` and `partials/footer.php` in
    `admin/index.php`.
    - Created `admin/partials/menu.php` to create the admin menu.
    - Created `admin/partials/footer.php` to create the admin footer.
    - Added `css/admin.css` file to style the admin pages.
    
    This commit implements the basic structure and styling of the admin
    dashboard, menu, footer, and CSS.

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<?php include('partials/menu.php'); ?>
+
+<!-- Main Content Section Starts -->
+<div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <br><br>
+                
+                <br><br>
+
+                <div class="col-4 text-center">
+
+                    
+
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+
+                    
+
+                    <br />
+                    Foods
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    
+
+                    <br />
+                    Total Orders
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    
+
+                    <br />
+                    Revenue Generated
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <!-- Main Content Section Ends -->
+
+<?php include('partials/footer.php') ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">Trang web được phát triển bởi Nhóm BatOn</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
