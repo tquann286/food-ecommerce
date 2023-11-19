2aebca9e75aeca2aae2961e76b1468ce4345b064
tquann286 <quantrung286@gmail.com>
2023-11-20 00:35:22 +0700

    feat: Add category form and manage category table UI

---
+++ admin/add-category.php
@@ -0,0 +1,148 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Add Category</h1>
+
+    <br><br>
+
+    <?php
+
+    if (isset($_SESSION['add'])) {
+      echo $_SESSION['add'];
+      unset($_SESSION['add']);
+    }
+
+    if (isset($_SESSION['upload'])) {
+      echo $_SESSION['upload'];
+      unset($_SESSION['upload']);
+    }
+
+    ?>
+
+    <br><br>
+
+    <!-- Add Category Form Starts -->
+    <form action="" method="POST" enctype="multipart/form-data">
+
+      <table class="tbl-30">
+        <tr>
+          <td>Title: </td>
+          <td>
+            <input type="text" name="title" placeholder="Category Title">
+          </td>
+        </tr>
+
+        <tr>
+          <td>Select Image: </td>
+          <td>
+            <input type="file" name="image">
+          </td>
+        </tr>
+
+        <tr>
+          <td>Featured: </td>
+          <td>
+            <input id="featured-yes" type="radio" name="featured" value="Yes"> 
+            <label for="featured-yes">Yes</label>
+            <input id="featured-No" type="radio" name="featured" value="No">
+            <label for="featured-No">No</label>
+          </td>
+        </tr>
+
+        <tr>
+          <td>Active: </td>
+          <td>
+            <input id="active-yes" type="radio" name="active" value="Yes"> 
+            <label for="active-yes">Yes</label>
+            <input id="active-No" type="radio" name="active" value="No">
+            <label for="active-No">No</label>
+          </td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <input type="submit" name="submit" value="Add Category" class="btn-secondary">
+          </td>
+        </tr>
+
+      </table>
+
+    </form>
+    <!-- Add CAtegory Form Ends -->
+
+    <?php
+
+    if (isset($_POST['submit'])) {
+      $title = $_POST['title'];
+
+      //For radio, we need to check whether the button is selected or not
+      if (isset($_POST['featured'])) {
+        $featured = $_POST['featured'];
+      } else {
+        $featured = "No";
+      }
+
+      if (isset($_POST['active'])) {
+        $active = $_POST['active'];
+      } else {
+        $active = "No";
+      }
+
+      // print_r($_FILES['image']);
+    
+      // die();
+    
+      if (isset($_FILES['image']['name'])) {
+        $image_name = $_FILES['image']['name'];
+
+        //Get the Extension of our image (jpg, png, gif, etc) e.g. "specialfood1.jpg"
+        $image_name_parts = explode('.', $image_name);
+        $ext = end($image_name_parts);
+
+        // Rename the Image
+        $image_name = "Food_Category_" . rand(000, 999) . '.' . $ext; // e.g. Food_Category_834.jpg
+
+        $source_path = $_FILES['image']['tmp_name'];
+
+        $destination_path = "../images/category/" . $image_name;
+
+        // Upload the Image
+        $upload = move_uploaded_file($source_path, $destination_path);
+
+        // Check whether the image is uploaded or not
+        // If the image is not uploaded then we will stop the process and redirect with error message
+        if ($upload == false) {
+          $_SESSION['upload'] = "<div class='error'>Failed to Upload Image. </div>";
+          header('location:' . SITEURL . 'admin/add-category.php');
+          die();
+        }
+      } else {
+        // Don't Upload Image and set the image_name value as blank
+        $image_name = "";
+      }
+
+      $sql = "INSERT INTO tbl_category SET 
+                    title='$title',
+                    image_name='$image_name',
+                    featured='$featured',
+                    active='$active'
+                ";
+
+      $res = mysqli_query($conn, $sql);
+
+      if ($res == true) {
+        $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
+        header('location:' . SITEURL . 'admin/manage-category.php');
+      } else {
+        $_SESSION['add'] = "<div class='error'>Failed to Add Category.</div>";
+        header('location:' . SITEURL . 'admin/add-category.php');
+      }
+    }
+
+    ?>
+
+  </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,10 +1,14 @@
-        <!-- Footer Section Starts -->
-        <div class="footer">
-            <div class="wrapper">
-                <p class="text-center">Trang web được phát triển bởi Nhóm BatOn</p>
-            </div>
-        </div>
-        <!-- Footer Section Ends -->
+<!-- Footer Section Starts -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">Trang web được phát triển bởi Nhóm BatOn</p>
+    </div>
+</div>
+<!-- Footer Section Ends -->
 
-    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
