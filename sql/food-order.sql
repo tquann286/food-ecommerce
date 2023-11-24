1dc03522e416fd1ced9d4006a45c63e6cc3ee184
tquann286 <quantrung286@gmail.com>
2023-11-24 23:15:06 +0700

    update sql

--- sql/food-order.sql
+++ sql/food-order.sql
@@ -45,7 +45,8 @@ INSERT INTO `tbl_admin` (`id`, `full_name`, `username`, `password`) VALUES
 (1, 'Đào Trung Quân', 'trungquan', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (9, 'Huỳnh Phương Thanh Phát ', 'thanhphat', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (10, 'Trương Nhật Huy', 'nhathuy', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
-(12, 'Nguyễn Hoàng Phước', 'hoangphuoc', '21232f297a57a5a743894a0e4a801fc3');
+(12, 'Nguyễn Hoàng Phước', 'hoangphuoc', '21232f297a57a5a743894a0e4a801fc3'),
+(13, 'A', 'sojemulap@mailinator.com', '7aa3262b9526ff30025c2f389263399e');
 
 -- --------------------------------------------------------
 
@@ -65,11 +66,11 @@ CREATE TABLE `tbl_category` (
 -- Dumping data for table `tbl_category`
 --
 
-INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
-(4, 'Pizza', 'Food_Category_790.jpg', 'Yes', 'Yes'),
-(5, 'Burger', 'Food_Category_344.jpg', 'Yes', 'Yes'),
-(6, 'MoMo', 'Food_Category_77.jpg', 'Yes', 'Yes'),
-(8, 'Quia est ipsum id id', 'Food_Category_929.jpg', 'No', 'Yes');
+-- INSERT INTO `tbl_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
+-- (4, 'Pizza', 'Food_Category_790.jpg', 'Yes', 'Yes'),
+-- (5, 'Burger', 'Food_Category_344.jpg', 'Yes', 'Yes'),
+-- (6, 'MoMo', 'Food_Category_77.jpg', 'Yes', 'Yes'),
+-- (8, 'Quia est ipsum id id', 'Food_Category_929.jpg', 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -92,13 +93,13 @@ CREATE TABLE `tbl_food` (
 -- Dumping data for table `tbl_food`
 --
 
-INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(3, 'Phở', 'Phở là một món ăn truyền thống của Việt Nam', '30000', 'Food-Name-3649.jpg', 6, 'Yes', 'Yes'),
-(4, 'Bánh Mì Ngon', 'Bánh mì với giò lụa, pate và nhiều phô mai.', '25000', 'Food-Name-6340.jpg', 5, 'Yes', 'Yes'),
-(5, 'Bánh Xèo', 'Bánh xèo chiên giòn, ăn kèm với rau sống.', '35000', 'Food-Name-8298.jpg', 4, 'No', 'Yes'),
-(6, 'Bún Riêu Cua', 'Bún riêu cua thơm ngon với nước dùng đậm đà.', '40000', 'Food-Name-7387.jpg', 6, 'Yes', 'Yes'),
-(7, 'Bánh Tráng Trộn', 'Bánh tráng trộn cay nồng với nhiều loại gia vị.', '20000', 'Food-Name-7833.jpg', 4, 'Yes', 'Yes'),
-(8, 'Cơm Niêu', 'Cơm niêu thơm ngon với các loại thịt và rau sống.', '52000', '', 5, 'No', 'No');
+-- INSERT INTO `tbl_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
+-- (3, 'Phở', 'Phở là một món ăn truyền thống của Việt Nam', '30000', 'Food-Name-3649.jpg', 6, 'Yes', 'Yes'),
+-- (4, 'Bánh Mì Ngon', 'Bánh mì với giò lụa, pate và nhiều phô mai.', '25000', 'Food-Name-6340.jpg', 5, 'Yes', 'Yes'),
+-- (5, 'Bánh Xèo', 'Bánh xèo chiên giòn, ăn kèm với rau sống.', '35000', 'Food-Name-8298.jpg', 4, 'No', 'Yes'),
+-- (6, 'Bún Riêu Cua', 'Bún riêu cua thơm ngon với nước dùng đậm đà.', '40000', 'Food-Name-7387.jpg', 6, 'Yes', 'Yes'),
+-- (7, 'Bánh Tráng Trộn', 'Bánh tráng trộn cay nồng với nhiều loại gia vị.', '20000', 'Food-Name-7833.jpg', 4, 'Yes', 'Yes'),
+-- (8, 'Cơm Niêu', 'Cơm niêu thơm ngon với các loại thịt và rau sống.', '52000', '', 5, 'No', 'No');
 
 
 -- --------------------------------------------------------
@@ -125,13 +126,13 @@ CREATE TABLE `tbl_order` (
 -- Dumping data for table `tbl_order`
 --
 
-INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
+-- INSERT INTO `tbl_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
 
-(1, 'Bún Riêu Cua', '40000', 3, '120000', '2023-02-15 03:49:48', 'Đã Huỷ', 'Nguyễn Thị An', '+84 912 345 678', 'nguyenthi.an@mailinator.com', '123 Đường ABC, Quận 1, TP.Hồ Chí Minh'),
+-- (1, 'Bún Riêu Cua', '40000', 3, '120000', '2023-02-15 03:49:48', 'Đã Huỷ', 'Nguyễn Thị An', '+84 912 345 678', 'nguyenthi.an@mailinator.com', '123 Đường ABC, Quận 1, TP.Hồ Chí Minh'),
 
-(2, 'Bánh Mì Ngon', '25000', 4, '100000', '2023-04-20 03:52:43', 'Đã giao', 'Trần Văn Bình', '+84 987 654 321', 'tranvanbinh@mailinator.com', '456 Đường XYZ, Quận 3, TP.Hồ Chí Minh'),
+-- (2, 'Bánh Mì Ngon', '25000', 4, '100000', '2023-04-20 03:52:43', 'Đã giao', 'Trần Văn Bình', '+84 987 654 321', 'tranvanbinh@mailinator.com', '456 Đường XYZ, Quận 3, TP.Hồ Chí Minh'),
 
-(3, 'Bánh Tráng Trộn', '20000', 2, '40000', '2023-08-10 04:07:17', 'Đã giao', 'Lê Thị Mai', '+84 933 222 111', 'lethimai@mailinator.com', '789 Đường KLM, Quận 5, TP.Hồ Chí Minh');
+-- (3, 'Bánh Tráng Trộn', '20000', 2, '40000', '2023-08-10 04:07:17', 'Đã giao', 'Lê Thị Mai', '+84 933 222 111', 'lethimai@mailinator.com', '789 Đường KLM, Quận 5, TP.Hồ Chí Minh');
 
 --
 -- Indexes for dumped tables
