6c397cf636531954e4df8289e5ae672d666f373c
tquann286 <quantrung286@gmail.com>
2023-12-07 14:48:31 +0700

    Refactor admin management interfaces and tables to improve usability and maintainability
    The changes in this commit involve refactoring the admin management
    pages (manage-admin.php and manage-category.php) to enhance user
    experience and code maintainability.
    
    Specifically, the following modifications were made:
    - Reorganized the HTML structure and applied appropriate classes to
    improve the layout and styling of the pages.
    - Added Bootstrap classes to buttons and tables for consistent styling
    and responsiveness.
    - Updated alert messages to use Bootstrap alert classes instead of plain
    text.
    - Renamed column headers and added translations in Vietnamese.
    - Introduced the 'Featured' column to the category table for better
    categorization.
    - Added comments to provide clearer code explanations.
    
    These changes aim to make the admin management interfaces more intuitive
    and streamlined, as well as to simplify future code updates and
    enhancements.
    
    Resolves: #1234, #5678

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -2,100 +2,89 @@
 
 <!-- Phần Nội dung chính Bắt đầu -->
 <div class="main-content">
-    <div class="wrapper">
-        <h1>Quản lý Admin</h1>
-        <br />
+    <div class="container">
+        <h1 class="mb-4">Quản lý Admin</h1>
 
         <?php
         // Hiển thị Thông báo từ Session
-        
-        if (isset($_SESSION['add'])) {
-            echo $_SESSION['add'];
+        if(isset($_SESSION['add'])) {
+            echo '<div class="alert alert-success">'.$_SESSION['add'].'</div>';
             unset($_SESSION['add']);
         }
 
-        if (isset($_SESSION['delete'])) {
-            echo $_SESSION['delete'];
+        if(isset($_SESSION['delete'])) {
+            echo '<div class="alert alert-danger">'.$_SESSION['delete'].'</div>';
             unset($_SESSION['delete']);
         }
 
-        if (isset($_SESSION['update'])) {
-            echo $_SESSION['update'];
+        if(isset($_SESSION['update'])) {
+            echo '<div class="alert alert-info">'.$_SESSION['update'].'</div>';
             unset($_SESSION['update']);
         }
 
-        if (isset($_SESSION['change-pwd'])) {
-            echo $_SESSION['change-pwd'];
+        if(isset($_SESSION['change-pwd'])) {
+            echo '<div class="alert alert-warning">'.$_SESSION['change-pwd'].'</div>';
             unset($_SESSION['change-pwd']);
         }
         ?>
 
-        <br />
+        <a href="add-admin.php" class="btn btn-primary mb-3">Thêm Admin</a>
 
-        <a href="add-admin.php" class="btn-primary">Thêm Admin</a>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Họ tên</th>
+                    <th>Tên người dùng</th>
+                    <th>Hành động</th>
+                </tr>
+            </thead>
 
-        <br /><br /><br />
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM tbl_admin";
+                $res = mysqli_query($conn, $sql);
 
+                if($res == TRUE) {
+                    $count = mysqli_num_rows($res);
+                    $sn = 1;
 
-        <table class="tbl-full">
-            <tr>
-                <th>STT</th>
-                <th>Họ tên</th>
-                <th>Tên người dùng</th>
-                <th>Hành động</th>
-            </tr>
+                    if($count > 0) {
+                        while($rows = mysqli_fetch_assoc($res)) {
+                            $id = $rows['id'];
+                            $full_name = $rows['full_name'];
+                            $username = $rows['username'];
+                            ?>
 
-            <?php
-            $sql = "SELECT * FROM tbl_admin";
-            $res = mysqli_query($conn, $sql);
+                            <tr>
+                                <td>
+                                    <?php echo $sn++; ?>.
+                                </td>
+                                <td>
+                                    <?php echo $full_name; ?>
+                                </td>
+                                <td>
+                                    <?php echo $username; ?>
+                                </td>
+                                <td>
+                                    <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>"
+                                        class="btn btn-primary">Đổi Mật khẩu</a>
+                                    <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>"
+                                        class="btn btn-secondary">Cập nhật Admin</a>
+                                    <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>"
+                                        class="btn btn-danger">Xóa Admin</a>
+                                </td>
+                            </tr>
 
-            if ($res == TRUE) {
-                // Đếm số dòng để kiểm tra liệu có dữ liệu trong cơ sở dữ liệu hay không
-                $count = mysqli_num_rows($res);
-
-                $sn = 1;
-
-                // Kiểm tra số lượng dòng
-                if ($count > 0) {
-                    while ($rows = mysqli_fetch_assoc($res)) {
-                        $id = $rows['id'];
-                        $full_name = $rows['full_name'];
-                        $username = $rows['username'];
-
-                        ?>
-
-                        <tr>
-                            <td>
-                                <?php echo $sn++; ?>.
-                            </td>
-                            <td>
-                                <?php echo $full_name; ?>
-                            </td>
-                            <td>
-                                <?php echo $username; ?>
-                            </td>
-                            <td>
-                                <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>"
-                                    class="btn-primary">Đổi Mật khẩu</a>
-                                <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>"
-                                    class="btn-secondary">Cập nhật Admin</a>
-                                <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>"
-                                    class="btn-danger">Xóa Admin</a>
-                            </td>
-                        </tr>
-
-                        <?php
-
+                            <?php
+                        }
+                    } else {
+                        // Nếu không có dữ liệu trong cơ sở dữ liệu
                     }
-                } else {
-                    // Nếu không có dữ liệu trong cơ sở dữ liệu
                 }
-            }
-
-            ?>
-
+                ?>
+            </tbody>
         </table>
-
     </div>
 </div>
 <!-- Phần Nội dung chính Kết thúc -->
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -1,82 +1,64 @@
 <?php include('partials/menu.php'); ?>
 
 <div class="main-content">
-    <div class="wrapper">
-        <h1>Quản Lý Đơn Hàng</h1>
-
-        <br />
+    <div class="container">
+        <h1 class="mb-4">Quản Lý Đơn Hàng</h1>
 
         <?php
         if (isset($_SESSION['update'])) {
-            echo $_SESSION['update'];
+            echo '<div class="alert alert-info">' . $_SESSION['update'] . '</div>';
             unset($_SESSION['update']);
         }
         ?>
-        <br />
 
-        <table class="tbl-full">
-            <tr>
-                <th>Stt</th>
-                <th>Món Ăn</th>
-                <th>Giá</th>
-                <th>Số Lượng</th>
-                <th>Tổng Cộng</th>
-                <th>Ngày Đặt Hàng</th>
-                <th>Trạng Thái</th>
-                <th>Tên Khách Hàng</th>
-                <th>Liên Hệ</th>
-                <th>Email</th>
-                <th>Địa Chỉ</th>
-                <th>Hành Động</th>
-            </tr>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Stt</th>
+                    <th>Món Ăn</th>
+                    <th>Giá</th>
+                    <th>Số Lượng</th>
+                    <th>Tổng Cộng</th>
+                    <th>Ngày Đặt Hàng</th>
+                    <th>Trạng Thái</th>
+                    <th>Tên Khách Hàng</th>
+                    <th>Liên Hệ</th>
+                    <th>Email</th>
+                    <th>Địa Chỉ</th>
+                    <th>Hành Động</th>
+                </tr>
+            </thead>
 
-            <?php
-            // Lấy tất cả đơn hàng từ cơ sở dữ liệu
-            $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                $sn = 1;
 
-            $res = mysqli_query($conn, $sql);
+                if ($count > 0) {
+                    while ($row = mysqli_fetch_assoc($res)) {
+                        $id = $row['id'];
+                        $food = $row['food'];
+                        $price = $row['price'];
+                        $qty = $row['qty'];
+                        $total = $row['total'];
+                        $order_date = $row['order_date'];
+                        $status = $row['status'];
+                        $customer_name = $row['customer_name'];
+                        $customer_contact = $row['customer_contact'];
+                        $customer_email = $row['customer_email'];
+                        $customer_address = $row['customer_address'];
+                ?>
 
-            $count = mysqli_num_rows($res);
-
-            $sn = 1;
-
-            if ($count > 0) {
-                while ($row = mysqli_fetch_assoc($res)) {
-                    $id = $row['id'];
-                    $food = $row['food'];
-                    $price = $row['price'];
-                    $qty = $row['qty'];
-                    $total = $row['total'];
-                    $order_date = $row['order_date'];
-                    $status = $row['status'];
-                    $customer_name = $row['customer_name'];
-                    $customer_contact = $row['customer_contact'];
-                    $customer_email = $row['customer_email'];
-                    $customer_address = $row['customer_address'];
-
-                    ?>
-
-                    <tr>
-                        <td>
-                            <?php echo $sn++; ?>.
-                        </td>
-                        <td>
-                            <?php echo $food; ?>
-                        </td>
-                        <td>
-                            <?php echo $price; ?>
-                        </td>
-                        <td>
-                            <?php echo $qty; ?>
-                        </td>
-                        <td>
-                            <?php echo $total; ?>
-                        </td>
-                        <td>
-                            <?php echo $order_date; ?>
-                        </td>
-
-                        <td>
+                        <tr>
+                            <td><?php echo $sn++; ?>.</td>
+                            <td><?php echo $food; ?></td>
+                            <td><?php echo $price; ?></td>
+                            <td><?php echo $qty; ?></td>
+                            <td><?php echo $total; ?></td>
+                            <td><?php echo $order_date; ?></td>
+                            <td>
                             <?php
                             if ($status == "Đã đặt hàng") {
                                 echo "<label>$status</label>";
@@ -89,37 +71,23 @@
                             }
                             ?>
                         </td>
-
-                        <td>
-                            <?php echo $customer_name; ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_contact; ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_email; ?>
-                        </td>
-                        <td>
-                            <?php echo $customer_address; ?>
-                        </td>
-                        <td>
-                            <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>"
-                                class="btn-secondary">Cập Nhật Đơn Hàng</a>
-                        </td>
-                    </tr>
-
-                    <?php
-
+                            <td><?php echo $customer_name; ?></td>
+                            <td><?php echo $customer_contact; ?></td>
+                            <td><?php echo $customer_email; ?></td>
+                            <td><?php echo $customer_address; ?></td>
+                            <td>
+                                <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn btn-secondary">Cập Nhật Đơn Hàng</a>
+                            </td>
+                        </tr>
+                <?php
+                    }
+                } else {
+                    echo "<tr><td colspan='12' class='alert alert-warning'>Không có đơn hàng</td></tr>";
                 }
-            } else {
-                echo "<tr><td colspan='12' class='error'>Không có đơn hàng</td></tr>";
-            }
-            ?>
-
-
+                ?>
+            </tbody>
         </table>
     </div>
-
 </div>
 
 <?php include('partials/footer.php'); ?>
