a4019b6457cd91cb843f01a1443268cf515960b2
tquann286 <quantrung286@gmail.com>
2023-12-07 14:48:06 +0700

    Refactor admin forms for better usability and accessibility
    Simplified and improved the design and functionality of the admin forms
    for adding admins, categories, and food items. Replaced the old
    table-based layout with a more modern and responsive layout using
    Bootstrap classes. Introduced form validation to ensure required fields
    are filled out and provided informative error messages. This revamp aims
    to enhance the user experience, making it easier to manage and add
    content to the system. Closes #123.

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -13,46 +13,44 @@
     }
     ?>
 
-    <form action="" method="POST">
+    <form action="" method="POST" class="needs-validation" novalidate>
 
-      <table class="tbl-30">
-        <tr>
-          <td>Họ và tên: </td>
-          <td>
-            <input type="text" name="full_name" placeholder="Nhập tên của bạn">
-          </td>
-        </tr>
+      <div class="row g-3">
+        <div class="col-md-6">
+          <label for="full_name" class="form-label">Họ và tên:</label>
+          <input type="text" name="full_name" class="form-control" id="full_name" placeholder="Nhập tên của bạn" required>
+          <div class="invalid-feedback">
+            Vui lòng nhập họ và tên.
+          </div>
+        </div>
 
-        <tr>
-          <td>Tên người dùng: </td>
-          <td>
-            <input type="text" name="username" placeholder="Tên người dùng của bạn">
-          </td>
-        </tr>
+        <div class="col-md-6">
+          <label for="username" class="form-label">Tên người dùng:</label>
+          <input type="text" name="username" class="form-control" id="username" placeholder="Tên người dùng của bạn" required>
+          <div class="invalid-feedback">
+            Vui lòng nhập tên người dùng.
+          </div>
+        </div>
 
-        <tr>
-          <td>Mật khẩu: </td>
-          <td>
-            <input type="password" name="password" placeholder="Mật khẩu của bạn">
-          </td>
-        </tr>
+        <div class="col-md-6">
+          <label for="password" class="form-label">Mật khẩu:</label>
+          <input type="password" name="password" class="form-control" id="password" placeholder="Mật khẩu của bạn" required>
+          <div class="invalid-feedback">
+            Vui lòng nhập mật khẩu.
+          </div>
+        </div>
 
-        <tr>
-          <td colspan="2">
-            <input type="submit" name="submit" value="Thêm Admin" class="btn-secondary">
-          </td>
-        </tr>
-
-      </table>
+        <div class="col-md-12">
+          <button type="submit" name="submit" class="btn btn-secondary">Thêm Admin</button>
+        </div>
+      </div>
 
     </form>
-
 
   </div>
 </div>
 
 <?php include('partials/footer.php'); ?>
-
 
 <?php
 
--- admin/index.php
+++ admin/index.php
@@ -2,79 +2,74 @@
 
 <!-- Phần Nội dung Chính Bắt Đầu -->
 <div class="main-content">
-    <div class="wrapper">
-        <h1>Trang chủ</h1>
-        <br><br>
+    <div class="container">
+        <h1 class="mt-3 mb-5">Trang chủ</h1>
+
         <?php
         if (isset($_SESSION['login'])) {
-            echo $_SESSION['login'];
+            echo '<div class="alert alert-success">' . $_SESSION['login'] . '</div>';
             unset($_SESSION['login']);
         }
         ?>
-        <br><br>
 
-        <div class="col-4 text-center">
-            <?php
-            $sql = "SELECT * FROM tbl_category";
-            $res = mysqli_query($conn, $sql);
-            $count = mysqli_num_rows($res);
-            ?>
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <?php
+                $sql = "SELECT * FROM tbl_category";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $count; ?></h1>
+                        <p class="card-text">Danh mục</p>
+                    </div>
+                </div>
+            </div>
 
-            <h1>
-                <?php echo $count; ?>
-            </h1>
-            <br />
-            Danh mục
+            <div class="col-md-3 mb-4">
+                <?php
+                $sql2 = "SELECT * FROM tbl_food";
+                $res2 = mysqli_query($conn, $sql2);
+                $count2 = mysqli_num_rows($res2);
+                ?>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $count2; ?></h1>
+                        <p class="card-text">Món ăn</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <?php
+                $sql3 = "SELECT * FROM tbl_order";
+                $res3 = mysqli_query($conn, $sql3);
+                $count3 = mysqli_num_rows($res3);
+                ?>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $count3; ?></h1>
+                        <p class="card-text">Đơn hàng</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <?php
+                $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Đã giao hàng'";
+                $res4 = mysqli_query($conn, $sql4);
+                $row4 = mysqli_fetch_assoc($res4);
+                $total_revenue = $row4['Total'];
+                ?>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 class="card-title"><?php echo $total_revenue ? $total_revenue . 'vnd' : '0vnd'; ?></h1>
+                        <p class="card-text">Doanh thu đã tạo</p>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="col-4 text-center">
-            <?php
-            $sql2 = "SELECT * FROM tbl_food";
-            $res2 = mysqli_query($conn, $sql2);
-            $count2 = mysqli_num_rows($res2);
-            ?>
-
-            <h1>
-                <?php echo $count2; ?>
-            </h1>
-            <br />
-            Món ăn
-        </div>
-
-        <div class="col-4 text-center">
-            <?php
-            $sql3 = "SELECT * FROM tbl_order";
-            $res3 = mysqli_query($conn, $sql3);
-            $count3 = mysqli_num_rows($res3);
-            ?>
-
-            <h1>
-                <?php echo $count3; ?>
-            </h1>
-            <br />
-            Tổng đơn hàng
-        </div>
-
-        <div class="col-4 text-center">
-            <?php
-            // Lấy tổng doanh thu
-            // Hàm tổng hợp trong SQL
-            $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Đã giao hàng'";
-            $res4 = mysqli_query($conn, $sql4);
-            $row4 = mysqli_fetch_assoc($res4);
-            $total_revenue = $row4['Total'];
-
-            ?>
-
-            <h1>
-                <?php echo $total_revenue; ?>vnd
-            </h1>
-            <br />
-            Doanh thu đã tạo
-        </div>
-
-        <div class="clearfix"></div>
-
     </div>
 </div>
 <!-- Phần Nội dung Chính Kết Thúc -->
