d76a6a85ad582a8b34d9ee509df479516fe66ebc
huynhthanhphat <thanhphat02122003@gmail.com>
2023-12-08 17:48:33 +0700

    config.php

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,6 @@
 <?php 
-    //Start Session
     session_start();
 
-
-    //Create Constants to Store Non Repeating Values
     define('SITEURL', 'http://localhost:3000/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
@@ -12,6 +9,5 @@
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn));
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error($conn));
-
 
 ?>
